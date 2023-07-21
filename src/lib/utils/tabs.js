9540ff3cdeaa673c20f8180e7cba7222789beae6
doctorcgs16 <122510960+doctorcgs16@users.noreply.github.com>
2023-07-20 21:30:25 -0400

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -22,58 +22,6 @@ export const tabs = [
         dest: '/rosters',
     },
     {
-        icon: 'view_comfy',
-        label: 'League Info',
-        nest: true,
-        children: [
-            {
-                icon: 'storage',
-                label: 'Rosters',
-                dest: '/rosters',
-            },
-            {
-                icon: 'groups',
-                label: 'Managers',
-                dest: '/managers',
-            },
-            {
-                icon: 'local_fire_department',
-                label: 'Rivalry',
-                dest: '/rivalry',
-            },
-            {
-                icon: 'leaderboard',
-                label: 'Standings',
-                dest: '/standings',
-            },
-            {
-                icon: 'view_comfy',
-                label: 'Drafts',
-                dest: '/drafts',
-            },
-            {
-                icon: 'emoji_events',
-                label: 'Trophy Room',
-                dest: '/awards',
-            },
-            {
-                icon: 'military_tech',
-                label: 'Records',
-                dest: '/records',
-            },
-            {
-                icon: 'history_edu',
-                label: 'Constitution',
-                dest: '/constitution',
-            },
-            {
-                icon: 'sports_football',
-                label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
-            },
-        ]
-    },
-    {
         icon: 'lightbulb',
         label: 'Go to Sleeper',
         dest: `https://sleeper.app/leagues/${leagueID}`,
